8fe0a8a0b1e29b33150a5c36ff1f498dc7cd1d46
pain2021053 <117257050+pain2021053@users.noreply.github.com>
2022-11-02 15:48:23 +0800

    0.1

--- BattleShip2.cpp
+++ BattleShip2.cpp
@@ -2,63 +2,81 @@
 #include <string>
 using namespace std;
 
-bool maps[3][3][3] = {
-    {
-        {0, 0, 0},
-        {0, 0, 0},
-        {0, 0, 0}
-    },
-    {
-        {0, 0, 0},
-        {0, 0, 0},
-        {0, 0, 0}
-    },
-    {
-        {0, 0, 0},
-        {0, 0, 0},
-        {0, 0, 0}
-    }
-};
-
-string user_map[3][3] = {
-    {"0", "0", "0"},
-    {"0", "0", "0"},
-    {"0", "0", "0"}
-};
-
-int row;
-int column;
-int hits;
-int choose_map;
-
-
-void print_map(int choose_map = 0){
-    for (int i; i < 3; i++) {
-        for (int ii; ii < 3; ii++) {
-            cout << maps[choose_map][i][ii];
-        }
-    }
-}
-
-
 int main() {
 
-    cout << "Please select your map :";
+    bool maps[3][3][3] = {
+        {
+            {0, 0, 0},
+            {0, 0, 0},
+            {0, 0, 0}
+        },
+        {
+            {0, 0, 0},
+            {0, 0, 0},
+            {0, 0, 0}
+        },
+        {
+            {0, 0, 0},
+            {0, 0, 0},
+            {0, 0, 0}
+        }
+    };
+
+    string user_map[3][3] = {
+        {"0", "0", "0"},
+        {"0", "0", "0"},
+        {"0", "0", "0"}
+    };
+
+    int row;
+    int column;
+    int hits;
+    int turns;
+    int choose_map;
+
+    //Ask user to select a map//
+
+    cout << "Please select a map (1/2/3) :";
     cin >> choose_map;
+    cout << "\n";
 
-    while (hits < 3)
-    {
-        cout << "\nPlease select a row :";
-        cin >> row;
+    //Print user's map//
 
-        cout << "\nPlease select a column :";
-        cin >> column;
-
-        
+    for (int i = 0; i < 3; i++) {
+        for (int ii = 0; ii < 3; ii++) {
+            cout << user_map[i][ii] << " ";
+        }
+    cout << '\n';
     }
-    
 
+    //Game mechanic//
 
+    while (hits < 3)  {
+
+        //Ask user to select target//
+
+        cout << '\n' << "Select a row :";
+        cin >> row;
+        cout << "\n";
+
+        cout << "Select a column :";
+        cin >> column;
+        cout << "\n";
+
+        //Check if user hit the target//
+
+        if (maps[choose_map][row][column] = true) {
+            maps[choose_map][row][column] = false;
+            hits++;
+            cout << "Hit! " << 3 - hits << " left.";
+        } else {
+            cout << "Miss! " << 3 - hits << " left.";
+        }
+
+        turns++;
+    }
+
+    cout << "You win!\n" << "You used " << turns << " to complete the game!";
 
     return 0;
 }
